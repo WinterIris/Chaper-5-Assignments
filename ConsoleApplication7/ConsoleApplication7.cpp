31cf9a389f25e2da5e1152bcb1474e7e304d93d6
Christopher Wilson <ckw82076@email.vccs.edu>
2025-04-07 08:11:53 -0400

    Update ConsoleApplication7.cpp

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -3,14 +3,13 @@ File Name : Chapter - 5 - Assignments
 Developer / Programmer : Christopher Wilson
 Date : 04 / 25
 
-Requirements : The pgram should display 2 random numbers to be added. The program should then pause while the student works on the problem. When the student is ready to check the answer, a key should be pressed to display the answer.
+Requirements : The program should follow the same steps as Math-Tutor-Part-1, but instead of a display of the answer, the program should wait for an answer to be entered. If the answer is correct, display a message of congratulatins. If not, display the message showing the correct answer.
 */
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <conio.h>
 
-    int main() {
+int main() {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
     int num1 = std::rand() % 100 + 1;
@@ -20,13 +19,18 @@ Requirements : The pgram should display 2 random numbers to be added. The progra
     std::cout << "Add the following numbers:\n";
     std::cout << "  " << num1 << "\n+ " << num2 << "\n" << std::endl;
 
-    std::cout << "Press any key when you're ready to see the answer...";
-    _getch();
+    int studentAnswer;
+    std::cout << "Enter your answer: ";
+    std::cin >> studentAnswer;
 
     int correctAnswer = num1 + num2;
 
-    std::cout << "\n\nThe correct answer is: " << correctAnswer << std::endl;
-    std::cout << "Great job practicing!" << std::endl;
+    if (studentAnswer == correctAnswer) {
+        std::cout << "Congratulations! That's correct!" << std::endl;
+    }
+    else {
+        std::cout << "Oops! The correct answer is " << correctAnswer << "." << std::endl;
+    }
 
     return 0;
 }
