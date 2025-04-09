098a8a2f0f4fc0ca58c47667e2d8cfc2df26a69f
Christopher Wilson <ckw82076@email.vccs.edu>
2025-04-09 07:55:32 -0400

    Update ConsoleApplication7.cpp

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -3,30 +3,47 @@ File Name : Chapter - 5 - Assignments
 Developer / Programmer : Christopher Wilson
 Date : 04 / 25
 
-Requirements : The pgram should display 2 random numbers to be added. The program should then pause while the student works on the problem. When the student is ready to check the answer, a key should be pressed to display the answer.
+Requirements : The program should prompts the user to enter the number of students in class, then loops to read that many names. Once all the names have been read, it reports which student would be at the front of the line and which one would be at the end. Assume no 2 students have the same name.
 */
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <conio.h>
+#include <string>
 
-    int main() {
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+int main() {
+    int numStudents;
 
-    int num1 = std::rand() % 100 + 1;
-    int num2 = std::rand() % 100 + 1;
+    do {
+        std::cout << "Enter the number of students in the class (1 to 25): ";
+        std::cin >> numStudents;
 
-    std::cout << "Welcome to the Math Tutor Program!\n\n";
-    std::cout << "Add the following numbers:\n";
-    std::cout << "  " << num1 << "\n+ " << num2 << "\n" << std::endl;
+        if (numStudents < 1 || numStudents > 25) {
+            std::cout << "Error: Number must be between 1 and 25.\n";
+        }
+    } while (numStudents < 1 || numStudents > 25);
 
-    std::cout << "Press any key when you're ready to see the answer...";
-    _getch();
+    std::string name, firstInLine, lastInLine;
 
-    int correctAnswer = num1 + num2;
+    std::cout << "\nEnter the first name of each student:\n";
 
-    std::cout << "\n\nThe correct answer is: " << correctAnswer << std::endl;
-    std::cout << "Great job practicing!" << std::endl;
+    for (int i = 0; i < numStudents; ++i) {
+        std::cout << "Student " << (i + 1) << ": ";
+        std::cin >> name;
+
+        if (i == 0) {
+            firstInLine = name;
+            lastInLine = name;
+        }
+        else {
+            if (name < firstInLine) {
+                firstInLine = name;
+            }
+            if (name > lastInLine) {
+                lastInLine = name;
+            }
+        }
+    }
+
+    std::cout << "\nStudent at the front of the line: " << firstInLine << std::endl;
+    std::cout << "Student at the end of the line: " << lastInLine << std::endl;
 
     return 0;
 }
